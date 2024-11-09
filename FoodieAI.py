879697b9f904db5da7cd85d06b57f294e8a3eeeb
andrewly7561 <andrewtl2006@gmail.com>
2024-11-09 14:54:44 -0500

    The first working version

--- FoodieAI.py
+++ FoodieAI.py
@@ -8,12 +8,20 @@ intermed = Intermediary()
 st.title("Being Vegetarian is :green[COOL] :sunglasses:")
 st.write("Are you a vegetarian and don't know where to find good vegetarian options. Don't worry VeggieAI got you covered.")
 with st.form("dietary_restrictions"):
+    # Stores the dietary restrictions as a list
     options = st.multiselect("What dietary restrictions do you have?",
     ["Vegetarian", "Gluten Free", "Low-Carb", "Nut Allergies", "Lactose Intolerant"])
-    st.text_input("Restaurant Website URL:")
+    
+    # Stores the URL of website
+    rst = st.text_input("Restaurant Website URL:")
+
     # Every form must have a submit button.
     submitted = st.form_submit_button("Submit")
+
     intermed.set_restrictions(options)
+    recommendation = intermed.input_website(rst)
+    st.header("Recommendations", divider = "gray")
+    st.write(recommendation)
 
     
 
