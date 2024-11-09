bff4990ab6babd0efccdfea7ee4569743f6f73fa
shlokbhattacharya <shlokbhattacharya4@gmail.com>
2024-11-09 14:59:45 -0500

    Fixed Website error

--- FoodieAI.py
+++ FoodieAI.py
@@ -1,9 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from intermediary import Intermediary
-
-intermed = Intermediary()
+from intermediary import *
 
 st.title("Being Vegetarian is :green[COOL] :sunglasses:")
 st.write("Are you a vegetarian and don't know where to find good vegetarian options. Don't worry VeggieAI got you covered.")
@@ -18,12 +16,12 @@ with st.form("dietary_restrictions"):
     # Every form must have a submit button.
     submitted = st.form_submit_button("Submit")
 
-    intermed.set_restrictions(options)
-    recommendation = intermed.input_website(rst)
+if submitted:
+    inter = Intermediary()
+    inter.set_restrictions(options)
     st.header("Recommendations", divider = "gray")
-    st.write(recommendation)
-
-    
+    st.write(inter.input_website(web))
+  
 
 restaurants = st.sidebar.text_input("Which restaurants would you like to eat at?")
 
