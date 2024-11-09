e6f881308a00db33abec73b37c914643e3ed998c
shlokbhattacharya <shlokbhattacharya4@gmail.com>
2024-11-09 17:57:15 -0500

    final submission

--- FoodieAI.py
+++ FoodieAI.py
@@ -5,26 +5,29 @@ st.title("Eat Easy")
 st.write('''Are you a vegetarian and don't know where to find good vegetarian options? Don't worry FoodieAI got you covered.
          We will find your favorite local restaurants and all their vegetarian friendly options.''')
 
+with st.form("dietary_restrictions"):
+    # Stores the dietary restrictions as a list
+    options = st.multiselect("What dietary restrictions do you have?",
+    ["Vegetarian", "Gluten Free", "Low-Carb", "Nut Allergies", "Lactose Intolerant"])
+
+    st.form_submit_button()
+
+inter = Intermediary()
+inter.set_restrictions(options)
+
 with st.form("location"):
     zipcode = st.text_input("What is your zipcode")
 
     submitted_1 = st.form_submit_button()
 
-inter = Intermediary()
-
 if submitted_1:
     # print(zipcode)
-    inter.set_restrictions([])
     st.header("Restaurant Recomendations", divider = "gray")
     # print(inter.input_zip(zipcode))
     st.write(inter.input_zip(zipcode))
 
-with st.form("dietary_restrictions"):
+with st.form("restaurant"):
     # Stores the dietary restrictions as a list
-    options = st.multiselect("What dietary restrictions do you have?",
-    ["Vegetarian", "Gluten Free", "Low-Carb", "Nut Allergies", "Lactose Intolerant"])
-    
-    # Stores the URL of website
     rst = st.text_input("Restaurant Name:")
 
     # Every form must have a submit button.
