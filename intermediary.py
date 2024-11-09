35ef272355fe3ec00cbc66b89595a902e17fe7b7
= <=>
2024-11-09 14:37:13 -0500

    Created intermediary class

---
+++ intermediary.py
@@ -0,0 +1,18 @@
+from main import Chatbot
+class Intermediary():
+    def __init__(self):
+        self.options = []
+        self.chatbot = None
+    
+    def set_restrictions(self, options):
+        self.chatbot = Chatbot(options)
+    
+    def input_website(self, link):
+        self.chatbot.chat(link)
+        return self.chatbot.get_bot_response()
+
+test = Intermediary()
+options = ["vegetarian"]
+test.set_restrictions(options)
+web = "https://sammamish.cafesinc.com/Userfiles/Docs/breakfastLunch2024.pdf"
+print(test.input_website(web))
--- main.py
+++ main.py
@@ -6,10 +6,19 @@ client = OpenAI(
     base_url="https://nova-litellm-proxy.onrender.com" # and this!!!
 )
 class Chatbot:
-    def __init__(self, client):
-        self.client = client
+    def __init__(self, options):
+        self.client = OpenAI(
+            api_key="sk-oDj6r8Ox2aQCQfBmxPh2IQ",
+            base_url="https://nova-litellm-proxy.onrender.com"
+        )
+        dietary_restrictions = ""
+        for i in options: 
+            dietary_restrictions += ", " + i
+
+        content = "You are an assistant, to help people find options for their dietary restrictions. Give all resopnses in concise bullet point form. The current dietary restrictions to search for are : " + dietary_restrictions
+
         self.context = [
-            {"role": "system", "content": "You are an assistant, to help people find options for their dietary restrictions. Give all resopnses in concise bullet point form."},
+            {"role": "system", "content": content},
         ]
         self.total_messages = 0;
         self.cur_message = []
@@ -36,12 +45,14 @@ class Chatbot:
         self.total_messages += 1
 
     def print_cur_message(self):
-        # print(self.context)
         for message in self.cur_message:
             if message["role"] == "user":
                 print(f'USER: {message["content"]}')
             elif message["role"] == "assistant":
                 print(f'BOT: {message["content"]}')
+    
+    def get_bot_response(self):
+        return self.cur_message[1]["content"]
     
     def print_chat(self):
         for message in self.context:
@@ -51,15 +62,16 @@ class Chatbot:
                 print(f'BOT: {message["content"]}')
 
 
+# options = ["vegetarian", "gluten free"]
 
-chatbot = Chatbot(client)
+# chatbot = Chatbot(options)
 
-client_input = input()
+# client_input = input()
 
-while(client_input != "quit"):
-    chatbot.chat(client_input)
-    chatbot.print_cur_message()
-    client_input = input()
-    # print(client_input)
+# while(client_input != "quit"):
+#     chatbot.chat(client_input)
+#     chatbot.print_cur_message()
+#     client_input = input()
+#     # print(client_input)
 
-print("exited loop")
+# print("exited loop")
